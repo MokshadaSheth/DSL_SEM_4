d3778924ec291fcf418dea4bf5f249edc2c6bea5
pict <shethmokshada@gmail.com>
2025-04-10 13:20:02 +0530

    MP Prac 8 added

---
+++ 21467_MP/Prac8.asm
@@ -0,0 +1,130 @@
+%macro rw 3
+mov rax, %1
+mov rdi,01
+mov rsi,%2
+mov rdx, %3
+syscall
+%endmacro
+
+section .data
+arr dq 30h,40h,50h,10h,70h
+dst dq 0,0,0,0,0
+
+m1 db 10,13,"Source:",10,13;
+l1 equ $-m1
+m2 db 10,13," Value:",10,13;
+l2 equ $-m2
+m3 db 10,13,"Destination:",10,13;
+l3 equ $-m3
+
+section .bss
+cnt resb 1
+count resq 1
+var resq 1
+ans resb 16
+
+
+global _start
+	section .text
+	
+	_start:
+	rw 01,m1,l1
+	mov byte[cnt],05h
+	mov rsi,arr
+	
+	repeat:
+	mov qword[var],rsi
+	
+	push rsi
+	call htoa
+	pop rsi
+	
+	mov rax,[rsi]   ;Content in RSI
+	mov qword[var],rax
+	
+	push rsi
+	
+	call htoa
+
+	pop rsi
+	
+	add rsi,08
+	dec byte[cnt]
+	jnz repeat
+ 
+ ;---Main logic
+ mov rsi,arr+32
+ mov rdi, arr+56
+ 
+ mov rcx, 05h
+ std    ;Direction flag 
+ rep movsq
+  
+ ;--------Destination
+ rw 01,m3,l3
+ mov byte[cnt],05h
+	mov rsi,dst-16 ;;IMP
+	
+	loop:
+	mov qword[var],rsi
+	
+	push rsi
+	call htoa
+	pop rsi
+	
+	mov rax,[rsi]   ;Content in RSI
+	mov qword[var],rax
+	
+	push rsi
+	call htoa
+
+	pop rsi
+	
+	add rsi,08
+	dec byte[cnt]
+	jnz loop
+ 
+ 
+ 
+ 
+jmp exit
+
+
+
+
+
+
+
+;---ASCII to HEX			
+htoa:
+	mov rax,qword[var]   ;Changing this
+	mov rbx,ans
+	mov byte[count],16
+	
+	again:
+	rol rax,04h
+	mov dl,al
+	and dl,0Fh
+	
+	cmp dl,09h
+	jle x
+	add dl,07h
+	
+	x:
+	add dl,30h
+	mov [rbx],dl
+	inc rbx
+	dec byte[count]
+	jnz again
+
+
+	rw 01,ans,16
+	rw 01,m2,l2
+	
+ret	
+				
+exit:
+mov rax,60
+mov rdi,00
+syscall	
+
