cd98f85c8fbee11b54e15b05b9d84e22b3a8b373
Mokshada_Sheth <shethmokshada@gmail.com>
2025-04-29 00:48:20 +0530

    Update Prac3.asm
    Solved bug

--- 21467_MP/Prac3.asm
+++ 21467_MP/Prac3.asm
@@ -31,6 +31,8 @@ mov al,00h ; Initially it is 0
 
 loop:
 	cmp al,[rdi]
+        ja incPointer         ;------Note:If jg 03 is largest as it considers CF as negative thus use ja
+
 	mov al,[rdi]  ;if al is not large then only
 	
 	incPointer:
@@ -78,4 +80,4 @@ exit:
 mov rax,60
 mov rdi,00
 syscall
-	ja incPointer         ;------Note:If jg 03 is largest as it considers CF as negative thus use ja
+	
