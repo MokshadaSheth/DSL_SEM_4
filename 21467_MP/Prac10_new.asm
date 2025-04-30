b9d08cca09320142bb5c6b75a78566543f0fd131
Mokshada_Sheth <shethmokshada@gmail.com>
2025-04-30 15:23:26 +0530

    Update Prac10_new.asm
    Recursive Factorial Implementation

--- 21467_MP/Prac10_new.asm
+++ 21467_MP/Prac10_new.asm
@@ -6,26 +6,88 @@ mov rdx, %3
 syscall
 %endmacro
 
-section .bss
-			argIp resb 1
-			result resb 4
-			
 section .data
-
-section .text
-
+    msg db "Enter number: ",10
+    msgLen equ $-msg
+    msg2 db "Factorial is: ",10
+    msg2Len equ $-msg2
+    fact1 db "1",10
+    fact1Len equ $-fact1
+    num db 05h
+    
+section .bss
+	ans resb 1
+    ;num resb 2
+    
 global _start
-
+section .text
+    
 _start:
+    ; rw 00,num,2
+    ; mov al, [num]
+    ; sub al, '0'
+    ; mov [num],bl
+    ; rw 01,num,2
 
-pop rbx
-pop rbx
-pop rbx
+    rw 01,msg,msgLen
+    mov bl, [num]
+    cmp bl,01h
+    jle fact_1
 
-mov [argIp],rbx;
-rw 1,argIp,1
+    call fact
+    mov byte[ans],al
+    call htoa
+    jmp exit
+
+fact_1:
+    rw 01,msg2,msg2Len
+    rw 01,fact1,fact1Len
+    jmp exit
+
+;-------Recursive Procedure (FIXED)
+fact:
+    cmp bl,1
+    jne cal_fact
+    mov ax,1
+    ret
+
+cal_fact:
+   push rbx
+   dec bl
+   call fact
+
+   pop rbx
+   mul bl
+   ret
+
+;----Hex to ASCII
+htoa:
+    mov al,byte[ans]
+    mov cl,2
+    mov rsi,ans
+again:
+    rol al,4
+    mov bl,al
+    and bl,0Fh
+
+    cmp bl,09h
+    jle add30
+    add bl,07h
+
+add30:
+    add bl,30h
+
+    mov [rsi], bl
+    inc rsi
+    dec cl
+    jnz again
+
+    rw 01,msg2,msg2Len
+    rw 01,ans,4
+
+    ret
 
 exit:
-mov rax,60
-mov rdi,00
-syscall	
+	mov rax,60
+	mov rdi,0
+	syscall
